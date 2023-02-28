57fec9d1c2ca35f3b40b950a5fc4ad7ecd7b3ebf
yeonjuan <yeonjuan93@naver.com>
2023-02-28 18:59:05 +0900

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
 import parseGitDiff from './parse-git-diff';
 export default parseGitDiff;
+
+export type {
+  AddedLine,
+  DeletedLine,
+  UnchangedLine,
+  MessageLine,
+  AnyLineChange,
+  ChunkRange,
+  Chunk,
+  CombinedChunk,
+  AnyChunk,
+  ChangedFile,
+  AddedFile,
+  DeletedFile,
+  RenamedFile,
+  AnyFileChange,
+  GitDiff,
+} from './types';
